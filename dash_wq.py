d2afb0846a20b5c88ffcfbf44c0fe5a188b28104
JorgeCH_01 <jchepote5412@gmail.com>
2025-08-01 00:31:18 -0500

    dash_wq.py

--- dash_wq.py
+++ dash_wq.py
@@ -271,4 +271,4 @@ def actualizar_heatmap(variable_destacada):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
